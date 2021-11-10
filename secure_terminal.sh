7ebef2667441a6219f3ad510e957cf8e96687a60
sorrynotsorry <54444533+sorrynotsorry1@users.noreply.github.com>
2021-11-10 11:39:11 +1100

    Update secure_terminal.sh

--- secure_terminal.sh
+++ secure_terminal.sh
@@ -10,7 +10,7 @@ do
 
 #blocks user after 4 attempts
 #----------------------------
-if [ ATTEMPT -gt 4 ]
+if [ $ATTEMPT -gt 4 ]
 then
 break
 fi
@@ -21,6 +21,7 @@ fi
 echo "Number of Attempts: $ATTEMPT"
 echo -n "Enter Password: "
 read -s PASSWORD_INPUT
+echo ""
 #----------------------------
 
 #gets the sha512 hash of the input from the user
@@ -30,7 +31,7 @@ PASSWORD=$(echo $PASSWORD_INPUT | sha512sum)
 
 #compares the inputed hashed password to this hash
 #----------------------------
-if [[ $PASSWORD = *"HASH"* ]];
+if [[ $PASSWORD = *"a059aeb4ee4520745e7b6f0c1ddffa0ca319988d821f4defd3359202cf80919655827b86437e741ed7121443ef924dc353f7e68de5751828f50598193143575e"* ]];
 then
 AUTH=$((1 + $AUTH))
 break
@@ -43,7 +44,7 @@ done
 
 #if the user is blocked they will be sent to this loop
 #----------------------------
-while [ AUTH = 0 ]
+while [ $AUTH = 0 ]
 do
 clear
 echo "Access Denied!"
@@ -53,22 +54,22 @@ done
 
 #if the user enters the right credentials they will be sent to this loop
 #----------------------------
-while [ AUTH = 1 ]
+while [ $AUTH = 1 ]
 do
 
 echo -n "> "
 read COMMAND
 #if the command has any of these phrases in it the command will not be exacuted
 #--------------------------------------------------------------
-if [[ $COMMAND = *"touch"* ]] || [[ $COMMAND = *"rm"* ]] || [[ $COMMAND = *"cat"* ]] || [[ $COMMAND = *"echo"* ]] || [[ $COMMAND = *"git"* ]] || [[ $COMMAND = *"apt"* ]] || [[ $COMMAND = *".sh"* ]] || [[ $COMMAND = *"sudo"* ]] || [[ $COMMAND = *"wget"* ]] || [[ $COMMAND = *"python"* ]] || [[ $COMMAND = *"make"* ]];
+if [[ $COMMAND = *"touch"* ]] || [[ $COMMAND = *"rm"* ]] || [[ $COMMAND = *"cat"* ]] || [[ $COMMAND = *"echo"* ]] || [[ $COMMAND = *"git"* ]] || [[ $COMMAND = *"apt"* ]] || [[ $COMMAND = *".sh"* ]] || [[ $COMMAND = *"sudo"* ]] || [[ $COMMAND = *"wget"* ]] || [[ $COMMAND = *"python"* ]] || [[ $COMMAND = *"make"* ]] || [[ $COMMAND = *"open"* ]];
 then
 echo "Command Invalid!"
 else
-TERMINAL_OUTPUT=$($COMMAND)
+TERMINAL_OUTPUT=$($COMMAND 2< /dev/null)
 
 #if the commands output shows the name of the file it will output "Invalid Output!"
 #----------------------------
-if [ TERMINAL_OUTPUT = *"secure_terminal.sh"* ]
+if [[ $TERMINAL_OUTPUT = *"secure_terminal.sh"* ]];
 then
 echo "Invalid Output!"
 else
